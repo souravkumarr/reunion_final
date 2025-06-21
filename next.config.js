a838b1e6e780de0e92652b0d46ad5fd578cbf630
Bitu@177 <souravkumarb8@gmail.com>
2025-06-21 17:55:37 +0530

    chore: Clean up webpack config after backend removal

--- next.config.js
+++ next.config.js
@@ -8,20 +8,7 @@ const nextConfig = {
     domains: ['localhost'],
   },
   webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Prevent undici from being bundled on the client side
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        undici: false,
-      };
-    }
-    
-    // Fix for undici module parsing issues
-    config.module.rules.push({
-      test: /node_modules\/undici/,
-      use: 'null-loader',
-    });
-    
+    // No longer needed after removing Firebase
     return config;
   },
 };
